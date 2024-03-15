da31d75ac80698d9e6dc2e078a41dd72df768ead
jvstingamboa <jvstingamboa@gmail.com>
2024-03-15 10:19:29 -0400

    added some filler

--- frontend/src/pages/Popup/Popup.jsx
+++ frontend/src/pages/Popup/Popup.jsx
@@ -37,6 +37,11 @@ const Popup = () => {
               <span className="font-medium text-gray-700">COT 4210 -- Homework 3</span>
               <span className='text-sm font-bold text-red-600'>43%</span>
             </li>
+            <li className='flex justify-between items-center bg-white shadow-sm px-4 py-3 border-l-4 border-blue-500 rounded hover:bg-blue-200 transition-colors duration-150 ease-in-out'>
+              <span className="font-medium text-gray-700">COP 4934 - Journal Week 5</span>
+              <span className='text-sm font-bold text-red-600'>18%</span>
+            </li>
+
             {/* Repeat for other list items */}
           </ul>
         </div>
@@ -48,8 +53,13 @@ const Popup = () => {
           <ul className="space-y-2  ">
             <li className='flex justify-between items-center bg-white shadow-sm px-4 py-3 border-l-4 border-blue-500 rounded hover:bg-blue-200 transition-colors duration-150 ease-in-out'>
               <span className="font-medium text-gray-700">COT 4210 -- Homework 3</span>
-              <span className='text-sm font-bold text-red-600'><a href="https://youtube.com" target='_blank'><img className='h-8 w-9' src={youtube} alt="youtube-logo" /></a></span>
+              <span className='text-sm font-bold text-red-600'><a href="https://youtube.com" target='_blank'><img className='h-6 w-9' src={youtube} alt="youtube-logo" /></a></span>
             </li>
+            <li className='flex justify-between items-center bg-white shadow-sm px-4 py-3 border-l-4 border-blue-500 rounded hover:bg-blue-200 transition-colors duration-150 ease-in-out'>
+              <span className="font-medium text-gray-700">COP 4934 - Journal Week 5</span>
+              <span className='text-sm font-bold text-red-600'><a href="https://youtube.com" target='_blank'><img className='h-6 w-9' src={youtube} alt="youtube-logo" /></a></span>
+            </li>
+
             {/* Repeat for other list items */}
           </ul>
           {/* Use similar styling for videos as assignments */}
