fa6100867f1e5eabe3db51b8ed37c2dcf55e0d4a
jvstingamboa <jvstingamboa@gmail.com>
2024-03-14 23:18:22 -0400

    refactored the styles to not make it so dark

--- frontend/src/pages/Popup/Popup.jsx
+++ frontend/src/pages/Popup/Popup.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import './Popup.css';
-
+import youtube from "../Popup/imgs/youtube.png"
 const Popup = () => {
   const [activeTab, setActiveTab] = useState('assignments');
+
+
+  const imgs = { youtube }
 
   return (
     <body className="bg-gray-100">
@@ -42,6 +45,13 @@ const Popup = () => {
       {activeTab === 'videos' && (
         <div className="max-w-2xl mx-auto">
           {/* Video-specific content here */}
+          <ul className="space-y-2  ">
+            <li className='flex justify-between items-center bg-white shadow-sm px-4 py-3 border-l-4 border-blue-500 rounded hover:bg-blue-200 transition-colors duration-150 ease-in-out'>
+              <span className="font-medium text-gray-700">COT 4210 -- Homework 3</span>
+              <span className='text-sm font-bold text-red-600'><a href="https://youtube.com" target='_blank'><img className='h-8 w-9' src={youtube} alt="youtube-logo" /></a></span>
+            </li>
+            {/* Repeat for other list items */}
+          </ul>
           {/* Use similar styling for videos as assignments */}
         </div>
       )}
